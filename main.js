7f54cae5263628f1e5da1feb9b1d5154b9ccd623
aarush0311 <72295580+aarush0311@users.noreply.github.com>
2021-04-08 17:11:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotposes);
+}
+function modelLoaded(){
+    console.log("poseNet is intialised");
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+    }
+
+}
+function draw(){
+    background("#0000FF");
+}
+
+
+
+
+
